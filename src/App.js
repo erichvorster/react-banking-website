57af9a2e7498066dff41789a90172b259604947a
erichvorster <erichvorster1@gmail.com>
2022-08-17 21:52:16 +0200

    3rd commit

--- src/App.js
+++ src/App.js
@@ -17,16 +17,20 @@ function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          {" "}
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          <Navbar />
           <Stats />,
           <Business />,
           <Billing />,
